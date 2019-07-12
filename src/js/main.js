0934b0d4002d69ef2e9fc2caff29289b8fe25d78
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2019-07-12 09:21:42 +0200

    feat: tabs js test

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
-"use strict";
 
-import moment from 'moment';
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+const tabLeft = document.querySelector('.navigation__edit--js');
+const tabRight = document.querySelector('.navigation__note--js');
+
+console.log(tabRight);
+
+const noteLeft = document.querySelector('.note--left--js');
+const noteRight = document.querySelector('.note--right--js');
+
+tabRight.addEventListener("click", (e) => {
+    noteLeft.style.display = "none";
+    noteRight.style.display = "block";
+})
