3d04d07c85d00a89ec065f71d04dbcbbc85e11f1
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2019-07-13 13:54:44 +0200

    fix: tabs opacity switch

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,22 @@
 
 const tabLeft = document.querySelector('.navigation__edit--js');
 const tabRight = document.querySelector('.navigation__note--js');
-
-console.log(tabRight);
-
 const noteLeft = document.querySelector('.note--left--js');
 const noteRight = document.querySelector('.note--right--js');
+const noteItem = document.querySelectorAll('.navigation__item--js');
 
 tabRight.addEventListener("click", (e) => {
     noteLeft.classList.add('note--invisable');
     noteRight.classList.remove('note--invisable');
+    noteItem[1].classList.remove('navigation__item--mobile');
+    noteItem[0].classList.add('navigation__item--mobile');
 });
 
 tabLeft.addEventListener("click", (e) => {
     noteLeft.classList.remove('note--invisable');
     noteRight.classList.add('note--invisable');
+    noteItem[0].classList.remove('navigation__item--mobile');
+    noteItem[1].classList.add('navigation__item--mobile');
 });
 
 const inputTitle = document.querySelector('.note__input--js');
