46da1eac39eb3fcc7be944484c33b9c292989974
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2019-07-13 13:24:12 +0200

    feat: localstorage actions

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,55 @@ tabLeft.addEventListener("click", (e) => {
     noteRight.classList.add('note--invisable');
 });
 
+const inputTitle = document.querySelector('.note__input--js');
+const inputText = document.querySelector('.note__textarea--js');
+const saveButton = document.querySelector('.note__save--js');
+const deleteButton = document.querySelector('.note__delete--js');
+const saveHeading = document.querySelector('.saved__heading--js');
+const saveText = document.querySelector('.saved__text--js');
+const unsaved = document.querySelector('.saved__unsaved--js');
+const savedContent = document.querySelector('.saved__content--js');
+
+
+inputTitle.addEventListener('keyup', (e) => {
+    localStorage.setItem('inputTitle', e.target.value);
+});
+
+inputText.addEventListener('keyup', (e) => {
+    localStorage.setItem('inputText', e.target.value);
+})
+
+if(localStorage.getItem('inputTitle') || localStorage.getItem('inputText')) {
+    inputTitle.value = localStorage.getItem('inputTitle');
+    inputText.value = localStorage.getItem('inputText');
+    
+}
+
+saveButton.addEventListener('click', (e) => {
+    saveHeading.innerHTML = inputTitle.value;
+    saveText.innerHTML = inputText.value;
+    localStorage.setItem('saveHeading', saveHeading.innerHTML);
+    localStorage.setItem('saveText', saveText.innerHTML)
+})
+
+if(localStorage.getItem('saveHeading')) {
+    saveHeading.innerHTML = localStorage.getItem('saveHeading');
+    saveText.innerHTML = localStorage.getItem('saveText');
+    unsaved.classList.add('saved__unsaved--hide');
+    savedContent.classList.remove('saved__unsaved--hide');
+}
+
+if(localStorage.getItem('saveText')) {
+    saveText.innerHTML = localStorage.getItem('saveText');
+    unsaved.classList.add('saved__unsaved--hide');
+    savedContent.classList.remove('saved__unsaved--hide');
+}
+
+deleteButton.addEventListener('click', (e) => {
+    localStorage.removeItem('inputTitle');
+    localStorage.removeItem('inputText');
+    localStorage.removeItem('saveHeading');
+    localStorage.removeItem('saveText');
+    inputTitle.value = "";
+    inputText.value = "";
+})
